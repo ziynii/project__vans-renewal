eb1328895e840eb3180efc6e0cdcb028ea10e88d
Jiyeon <yeeoni_@naver.com>
2022-03-22 19:04:22 +0900

    🚧 Implement main-banner carousel in all size

---
+++ js/carousel.js
@@ -0,0 +1,19 @@
+const mainBannerMobileSlide = tns({
+  container: '.main-banner-slide-md .slide-list',
+  items: 1,
+  slideBy: 'page',
+  autoplay: true,
+  autoplayButtonOutput: false,
+  nav: false,
+  controlsContainer: '.main-banner-slide-md .slide-controls',
+});
+
+const mainBannerDesktopSlide = tns({
+  container: '.main-banner-slide-xl .slide-list',
+  items: 1,
+  slideBy: 'page',
+  autoplay: true,
+  autoplayButtonOutput: false,
+  nav: false,
+  controlsContainer: '.main-banner-slide-xl .slide-controls',
+});
